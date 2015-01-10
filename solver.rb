9710f38fab1325c9c211890af6e68ad1c27330ec
Jeffrey Gu <jeffwgu@gmail.com>
2015-01-10 09:00:18 -0700

    integrated dj and my changes. improved logic. solves boards. will try harder ones

--- solver.rb
+++ solver.rb
@@ -6,7 +6,7 @@ class Solver
     @solution = []
   end
 
-  def intake_board(boardfile)
+  def board_intake(boardfile)
     @board = []
     File.readlines(boardfile).each do |line|
       game_row = line.delete("\n").split("")
@@ -18,7 +18,7 @@ class Solver
 
   def spot_make
     @board.each_with_index do |row, row_index|
-      row.each_with_index {|entry, column_index| row[column_index] = Spot.new([row_index, column_index], self.square_finder([row_index, column_index])) if entry == 0}
+      row.each_with_index {|entry, column_index| row[column_index] = Spot.new(row_index, column_index, self.square_finder(row_index, column_index)) if entry == 0}
     end
   end
 
@@ -42,6 +42,7 @@ class Solver
     end
   end
 
+
   def squarer(square_number, array_set)
     squared_array = []
     case square_number
@@ -51,48 +52,89 @@ class Solver
     end
     squared_array.flatten
   end
-  
-  def square_finder(coordinate)
-    case coordinate[0]
+
+  def square_finder(row_index, column_index)
+    case row_index
     when 0..2
-      case coordinate[1]
+      case column_index
         when 0..2 then 1
         when 3..5 then 2
         when 6..8 then 3
       end
-    when 3..5 
-      case coordinate[1]
+    when 3..5
+      case column_index
         when 0..2 then 4
         when 3..5 then 5
         when 6..8 then 6
       end
-    when 6..8 
-      case coordinate[1]
+    when 6..8
+      case column_index
         when 0..2 then 7
         when 3..5 then 8
         when 6..8 then 9
       end
     end
   end
-  
+
   def candidate_delete(spot, chunk)
     spot.candidates = spot.candidates.reject {|x| chunk.include?(x)}
   end
 
   def chunk_check(spot)
-    self.candidate_delete(spot, @board[spot.coordinates[0]])
-    self.candidate_delete(spot, column_make(spot.coordinates[1]))
+    self.candidate_delete(spot, @board[spot.row_index])
+    self.candidate_delete(spot, column_make(spot.column_index))
     self.candidate_delete(spot, square_make(spot.square))
+  end
+
+  def candidate_alone?(spot)
+    spot.candidates.length == 1
+  end
+
+  def spot_remove(spot)
+    @board[spot.row_index][spot.column_index] = spot.candidates[0]
+  end
+
+
+  def spot_scan
+    while self.board_clean?
+      @board.each do |row|
+        row.each { |entry| self.sudoku_solve(entry) if entry.is_a?(Spot) }
+      end
+    end
+  end
+
+  def board_solved?
+      @board.flatten.reduce(:+) == 405 ? puts("Solution correct. You just got robodoku'd.") : puts("Loser.")
+  end
+
+  def board_clean?
+    @board.flatten.any? {|entry| entry.is_a?(Spot)} ? true : self.board_solved? 
+  end
+
+  def sudoku_solve(entry)
+      self.chunk_check(entry)
+      self.spot_remove(entry) if candidate_alone?(entry)
   end
 end
 
+
+    # chunk_check to reduce cands by row, square, column
+    # scan through spots and spot_remove
+    # somehow scan and check if cands no longer decreasing
+    # then
+    # do harder algorithm to reduce cands further
+    #  then
+    # prior algorithm
+
+
 class Spot
   attr_accessor :candidates
-  attr_reader :coordinates, :square
+  attr_reader :row_index, :column_index, :square
 
-  def initialize(coordinates,square)
-    @coordinates = coordinates
-    @square = square 
+  def initialize(row_index, column_index, square)
+    @row_index = row_index
+    @column_index = column_index
+    @square = square
     @candidates = (1..9).to_a
   end
 end
--- solver_test.rb
+++ solver_test.rb
@@ -8,7 +8,7 @@ class SolverTest < MiniTest::Test
 
   def setup
     @solver = Solver.new
-    @solver.intake_board("board_2.txt")
+    @solver.board_intake("board_2.txt")
     @solver.spot_make
   end
 
@@ -20,48 +20,39 @@ class SolverTest < MiniTest::Test
     skip
     puts @solver.board[0]
   end
-  
+
   def test_it_finds_the_right_square
-    square_3 = [0, 6]
-    square_5 = [3, 3]
-    square_8 = [8, 3]
-    assert_equal 3, @solver.square_finder(square_3)
-    assert_equal 5, @solver.square_finder(square_5)
-    assert_equal 8, @solver.square_finder(square_8)
+    assert_equal 3, @solver.square_finder(0, 6)
+    assert_equal 5, @solver.square_finder(3, 3)
+    assert_equal 8, @solver.square_finder(8, 3)
   end
 
   def test_column_includes_spot_number_and_not_number
-    @solver.intake_board("board_4.txt")
+    @solver.board_intake("board_4.txt")
     result_column = @solver.column_make(4)
-    refute result_column.include?(7) 
+    refute result_column.include?(7)
     assert result_column.include?(8)
-    assert result_column[1].is_a?(Spot)  
+    assert result_column[1].is_a?(Spot)
   end
 
   def test_row_includes_spot_number_and_not_number
-    @solver.intake_board("board_4.txt")
+    @solver.board_intake("board_4.txt")
     row = @solver.board[2]
-    refute row.include?(5) 
+    refute row.include?(5)
     assert row.include?(3)
-    assert row[7].is_a?(Spot)  
-  end
- 
-  def test_square_includes_spot_number_and_not_number
-    @solver.intake_board("board_4.txt")
-    result_square = @solver.square_make(4)
-    refute result_square.include?(5) 
-    assert result_square.include?(7)
-    assert result_square[4].is_a?(Spot)  
+    assert row[7].is_a?(Spot)
   end
 
-  def test_it_outputs_the_correct_square
-    puts @solver.square_make(2)    
-    puts "\n"
-    puts @solver.square_make(5)
+  def test_square_includes_spot_number_and_not_number
+    @solver.board_intake("board_4.txt")
+    result_square = @solver.square_make(4)
+    refute result_square.include?(5)
+    assert result_square.include?(7)
+    assert result_square[4].is_a?(Spot)
   end
-  
+
   def test_solver_candidate_delete_arrays
-    spot = Spot.new(nil,nil)
+    spot = Spot.new(nil,nil,nil)
     assert_equal [4,5,6,7,8,9], @solver.candidate_delete(spot,[1,2,3])
     assert_equal [4,5,6,7,8,9], spot.candidates
   end
@@ -72,13 +63,31 @@ class SolverTest < MiniTest::Test
   end
 
   def test_it_chunk_checks
-    @solver.intake_board("board_3.txt")
+    @solver.board_intake("board_3.txt")
     candidates_left = []
-    candidates_left << @solver.chunk_check(@solver.board[0][1])  
-    candidates_left << @solver.chunk_check(@solver.board[1][4])  
-    candidates_left << @solver.chunk_check(@solver.board[2][7])  
-    candidates_left << @solver.chunk_check(@solver.board[3][5])  
-    candidates_left << @solver.chunk_check(@solver.board[4][1])  
+    candidates_left << @solver.chunk_check(@solver.board[0][1])
+    candidates_left << @solver.chunk_check(@solver.board[1][4])
+    candidates_left << @solver.chunk_check(@solver.board[2][7])
+    candidates_left << @solver.chunk_check(@solver.board[3][5])
+    candidates_left << @solver.chunk_check(@solver.board[4][1])
     assert_equal [9,7,5,1,5], candidates_left.flatten
+  end
+
+  def test_it_removes_a_spot_correctly
+    @solver.board_intake("board_3.txt")
+    @solver.chunk_check(@solver.board[0][1])
+    assert_equal 9, @solver.spot_remove(@solver.board[0][1])
+  end
+
+  def test_it_solves_Jeffs_board
+    @solver.board_intake("board_2.txt")
+    @solver.spot_scan
+    @solver.board_clean?
+  end
+  
+  def test_it_solves_easy_board
+    @solver.board_intake("board_4.txt")
+    @solver.spot_scan
+    @solver.board_clean?
   end
 end
