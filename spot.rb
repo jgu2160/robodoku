2d32de453a0aa101e2124b95fe2e2ac98b62f378
DJ Greenfield <mr.kaitryn.dale@gmail.com>
2015-01-13 17:23:20 -0700

    spliced spot into its own file

---
+++ spot.rb
@@ -0,0 +1,11 @@
+class Spot
+  attr_accessor :candidates
+  attr_reader :row_index, :column_index, :square
+
+  def initialize(row_index, column_index, square)
+    @row_index = row_index
+    @column_index = column_index
+    @square = square
+    @candidates = (1..9).to_a
+  end
+end
--- sudoku_solver.rb
+++ sudoku_solver.rb
@@ -13,15 +13,3 @@ class Solver
 #better solver via medium_algo
 #best solver via swordfish
 end
-
-class Spot
-  attr_accessor :candidates
-  attr_reader :row_index, :column_index, :square
-
-  def initialize(row_index, column_index, square)
-    @row_index = row_index
-    @column_index = column_index
-    @square = square
-    @candidates = (1..9).to_a
-  end
-end
