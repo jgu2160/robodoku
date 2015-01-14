ec413259828ef1325c812b21b8fddab5bdddc550
DJ Greenfield <mr.kaitryn.dale@gmail.com>
2015-01-13 17:22:17 -0700

    added swordfish.rb with comments for objectives

---
+++ swordfish.rb
@@ -0,0 +1,14 @@
+class Swordfish
+
+#   choose spot, choose candidate; fish = candidate
+#   detect 2nd spot by 1st spot[row, fish]
+#   detect 3rd spot by 2nd spot[column, fish]
+#   detect 4th spot by 3rd spot[row, fish]
+#   detect 5th spot by 4th spot[column, fish]
+#   detect 6th spot by 5th spot[row, fish]
+#   acknowledge 1st spot by 6th spot[column=spot1's=column, fish]
+# => then reject fish from all non fishy spots which share fishy rows OR columns
+
+
+
+end
